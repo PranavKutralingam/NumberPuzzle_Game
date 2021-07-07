c004a86c23a496f58351b4b62a67f567c33e334f
PranavKutralingam <thirupranav99@gmail.com>
2021-07-07 18:47:14 +0530

    solved

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -37,15 +37,41 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		Button[] buttons = game.getButtons();
 		
 		//Your logic here		
-		
+		int idx=0;
+		for(int i=0;i<=15;i++) {
+			if(buttons[i]==buttonClicked) {
+				idx=i;
+				break;
+			}
+		}
+		if((idx+1==emptyCellId && (idx+1)%4!=0) || idx-1 ==emptyCellId && (idx%4!=0)|| idx+4==emptyCellId || idx-4==emptyCellId) {
+			swapButton(buttons[emptyCellId],buttonClicked);
+			return idx;		
+		}
 		return emptyCellId;
 
+	}
+	public boolean search(int[]arr,int a) {
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==a)
+				return false;
+		}
+		return true;
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
 		
 		//Your logic here
-		int a = getRandomNumber();
+
+		int i=0;
+		while(i<arr.length) {
+			int a = getRandomNumber();
+			if(a<=15 && search(arr,a)) {
+				arr[i]=a;
+				i++;
+			}
+		}
+		
 		
 		
 		return arr;
@@ -55,7 +81,14 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
+		int arr[]=getIntegerArrayOfButtonIds(buttons);
+		int prev=arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(prev>arr[i]) {
+				winner=false;
+			}
+			prev=arr[i];
+		}
 
 		return winner;
 	}
